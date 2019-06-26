27eb60f9e0e74f9639023de6b0b07f12f7dfeb4d
Olivier Beaulieu <olivierbeaulieu657@gmail.com>
2019-06-26 16:01:57 -0400

    Basic (buggy) running setup with an action reader

--- example/index.ts
+++ example/index.ts
@@ -3,9 +3,9 @@
  * Watcher with both of those.
  */
 
-import { BaseActionWatcher } from "demux"
-import { DfuseActionReader } from "../src/"
+import { DfuseActionReader } from "../src"
 import dotenv from "dotenv"
+import { BaseActionWatcher } from "demux"
 
 dotenv.config()
 
@@ -30,21 +30,14 @@ const actionHandler = new ObjectActionHandler([handlerVersion])
  * The second argument defines at what block this should start at. For values less than 1, this switches to a "tail"
  * mode, where we start at an offset of the most recent blocks.
  */
-const actionReader = new DfuseActionReader({
-  startAtBlock: 65414000,
+const dfuseActionReader = new DfuseActionReader({
+  startAtBlock: 50000000, // default is 1, which means start at genesis block
   onlyIrreversible: false,
   dfuseApiKey: process.env.DFUSE_API_KEY as string,
   network: "mainnet"
 })
 
-/* BaseActionWatcher
- * This ready-to-use base class helps coordinate the Action Reader and Action Handler, passing through block information
- * from the Reader to the Handler. The third argument is the polling loop interval in milliseconds. Since EOS has 0.5s
- * block times, we set this to half that for an average of 125ms latency.
- *
- * All that is left to run everything is to call `watch()`.
- */
-const actionWatcher = new BaseActionWatcher(actionReader, actionHandler, 250)
+const actionWatcher = new BaseActionWatcher(dfuseActionReader, actionHandler, 125)
 
 actionWatcher.watch()
 
--- src/index.ts
+++ src/index.ts
@@ -13,30 +13,66 @@ import { gql, InMemoryCache } from "apollo-boost"
 import fetch from "node-fetch"
 import WebSocketConnection from "ws"
 import { WebSocketLink } from "apollo-link-ws"
+import { SubscriptionClient } from "subscriptions-transport-ws"
 
 type DfuseActionReaderOptions = ActionReaderOptions & {
   dfuseApiKey: string
   network?: string
 }
 
+const defaultBlock: Block = {
+  blockInfo: {
+    blockNumber: 0,
+    blockHash: "",
+    previousBlockHash: "",
+    timestamp: new Date(0)
+  },
+  actions: []
+}
+const block1: Block = {
+  blockInfo: {
+    blockNumber: 1,
+    blockHash: "00000001405147477ab2f5f51cda427b638191c66d2c59aa392d5c2c98076cb0",
+    previousBlockHash: "",
+    timestamp: new Date("2018-06-08T08:08:08.000Z")
+  },
+  actions: []
+}
+const block2: Block = {
+  blockInfo: {
+    blockNumber: 2,
+    blockHash: "0000000267f3e2284b482f3afc2e724be1d6cbc1804532ec62d4e7af47c30693",
+    previousBlockHash: "00000001405147477ab2f5f51cda427b638191c66d2c59aa392d5c2c98076cb0",
+    timestamp: new Date("2018-06-08T08:08:08.000Z")
+  },
+  actions: []
+}
+
 export class DfuseActionReader extends AbstractActionReader {
   protected network: string
   protected dfuseApiKey: string
-  protected dfuseClient?: DfuseClient
-  protected apolloClient?: ApolloClient<any>
+  protected apolloClient: ApolloClient<any>
   protected headInfoInitialized: boolean = false
+  protected activeCursor: string = ""
+  protected blockQueue: Block[] = []
 
   constructor(options: DfuseActionReaderOptions) {
     super(options)
     this.dfuseApiKey = options.dfuseApiKey
     this.network = options.network || "mainnet"
 
+    // Patch for an issue where dfuse doesnt return blocks 1 and 2
+    if (this.startAtBlock > 0 && this.startAtBlock < 3) {
+      this.startAtBlock = 3
+      this.currentBlockNumber = 2
+    }
+
     /*
      * todo: This forcibly configures the client to run on node.
      * What if the user wants to run on browser? Maybe allow to
      * pass in a dfuse client instance in the constructor?
      */
-    this.dfuseClient = createDfuseClient({
+    const dfuseClient = createDfuseClient({
       apiKey: this.dfuseApiKey,
       network: this.network,
       httpClientOptions: {
@@ -62,50 +98,153 @@ export class DfuseActionReader extends AbstractActionReader {
       }
     })
 
-    this.apolloClient = new ApolloClient({
-      cache: new InMemoryCache(),
-      link: new WebSocketLink({
-        uri: this.dfuseClient.endpoints.graphqlStreamUrl,
-        options: {
-          reconnect: true,
-          connectionParams: async () => {
-            const apiToken = await this.dfuseClient!.getTokenInfo()
-
-            return {
-              Authorization: `Bearer ${apiToken.token}`
-            }
+    const subscriptionClient = new SubscriptionClient(
+      dfuseClient.endpoints.graphqlStreamUrl,
+      {
+        reconnect: true,
+        connectionCallback: (error?: any) => {
+          if (error) {
+            console.log("Unable to correctly initialize connection", error)
+            process.exit(1)
           }
         },
-        webSocketImpl: WebSocketConnection
-      })
+        connectionParams: async () => {
+          const { token } = await dfuseClient.getTokenInfo()
+
+          return {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      },
+      WebSocketConnection
+    )
+
+    subscriptionClient.onConnecting(() => {
+      console.log("Connecting")
     })
+    subscriptionClient.onConnected(() => {
+      console.log("Connected")
+    })
+    subscriptionClient.onReconnecting(() => {
+      console.log("Reconnecting")
+    })
+    subscriptionClient.onReconnected(() => {
+      console.log("Reconnected")
+    })
+    subscriptionClient.onDisconnected(() => {
+      console.log("Disconnected")
+    })
+    subscriptionClient.onError((error) => {
+      console.log("Error", error)
+    })
+
+    this.apolloClient = new ApolloClient({
+      cache: new InMemoryCache(),
+      link: new WebSocketLink(subscriptionClient)
+    })
+
+    // this.streamHeadInfo((params) => {
+    //   this.headBlockNumber = params.head_block_num
+    //   this.lastIrreversibleBlockNumber = params.last_irreversible_block_num
+    // })
+
+    this.streamTransactions()
+  }
+
+  private async streamTransactions() {
+    let currentBlockNumber: number = -1
+    let currentBlock: Block
 
     this.apolloClient
       .subscribe({
         query: gql`
-          subscription {
-            searchTransactionsForward(query: "status:executed") {
+          subscription($cursor: String!, $lowBlockNum: Int64) {
+            searchTransactionsForward(
+              query: "status:executed"
+              cursor: $cursor
+              lowBlockNum: $lowBlockNum
+            ) {
+              irreversibleBlockNum
               trace {
+                id
+                block {
+                  num
+                  id
+                  previous
+                  timestamp
+                }
                 matchingActions {
-                  receiver
                   account
                   name
-                  json
+                  data
+                  authorization {
+                    actor
+                    permission
+                  }
                 }
               }
             }
           }
-        `
+        `,
+        variables: {
+          cursor: this.activeCursor,
+          lowBlockNum: this.currentBlockNumber
+        }
       })
       .subscribe({
         start: (subscription) => {
           console.log("Started", subscription)
         },
         next: (value) => {
-          const trace = value.data.searchTransactionsForward.trace
-          trace.matchingActions.forEach((action: any) => {
-            console.log(`Action ${action.receiver}/${action.account}:${action.name}`)
+          const message = value.data.searchTransactionsForward
+          const { trace, irreversibleBlockNum } = message
+          const { matchingActions, block } = trace
+
+          const isFirstProcessed = currentBlockNumber === -1
+          const isNewBlock = block.num !== currentBlockNumber
+
+          if (isNewBlock) {
+            this.headBlockNumber = Math.max(this.headBlockNumber, currentBlockNumber)
+          }
+
+          if (!isFirstProcessed && isNewBlock) {
+            // console.log(`Finished processing block #${currentBlockNumber}, starting #${block.num}`)
+            this.blockQueue.push(currentBlock)
+          }
+
+          this.lastIrreversibleBlockNumber = Math.max(
+            this.lastIrreversibleBlockNumber,
+            irreversibleBlockNum
+          )
+
+          if (isNewBlock) {
+            currentBlockNumber = message.trace.block.num
+            currentBlock = {
+              actions: [],
+              blockInfo: {
+                blockNumber: block.num,
+                blockHash: block.id,
+                previousBlockHash: block.previous,
+                timestamp: block.timestamp
+              }
+            }
+          }
+
+          matchingActions.forEach((action: any) => {
+            currentBlock.actions.push({
+              type: `${action.account}::${action.name}`,
+              payload: {
+                transactionId: trace.id,
+                actionIndex: 0,
+                account: action.account,
+                name: action.name,
+                authorization: action.authorization,
+                data: action.data
+              }
+            })
           })
+
+          this.activeCursor = message.cursor
         },
         error: (error) => {
           console.log("Error", error)
@@ -114,73 +253,62 @@ export class DfuseActionReader extends AbstractActionReader {
           console.log("Completed")
         }
       })
-
-    this.streamHeadInfo((params) => {
-      this.headBlockNumber = params.head_block_num
-      this.lastIrreversibleBlockNumber = params.last_irreversible_block_num
-    })
   }
 
-  protected async streamHeadInfo(onInfo: (headInfo: HeadInfoData) => void): Promise<void> {
-    // todo Should the stream be closed, ever?
-    await this.dfuseClient!.streamHeadInfo(async (message: InboundMessage) => {
-      if (message.type === InboundMessageType.HEAD_INFO) {
-        onInfo(message.data)
-      }
-    })
+  // protected async streamHeadInfo(onInfo: (headInfo: HeadInfoData) => void): Promise<void> {
+  //   // todo Should the stream be closed, ever?
+  //   await this.dfuseClient!.streamHeadInfo(async (message: InboundMessage) => {
+  //     if (message.type === InboundMessageType.HEAD_INFO) {
+  //       onInfo(message.data)
+  //     }
+  //   })
 
-    return
-  }
+  //   return
+  // }
 
   protected async setup(): Promise<void> {}
 
   public async getBlock(blockNumber: number): Promise<Block> {
-    console.log("blockNumber", blockNumber)
-    console.log("head", this.headBlockNumber)
-    console.log("last", this.lastIrreversibleBlockNumber)
-    console.log("-=====")
+    await waitUntil(() => this.blockQueue.length > 0)
+    // console.log("getBlock", blockNumber, this.headBlockNumber, this.lastIrreversibleBlockNumber)
+    // console.log("in queue", this.blockQueue.map((x) => x.blockInfo.blockNumber))
+    const queuedBlock = this.blockQueue[0]
+    const queuedBlockNumber = queuedBlock.blockInfo.blockNumber
 
-    // const block = result.data.searchTransactionsBackward.results[0].trace.block
+    if (blockNumber === 1) {
+      return block1
+    }
+    if (blockNumber === 2) {
+      return block2
+    }
 
-    // return {
-    //   actions: [
-    //     /*{
-    //     type: 'eosio.token::transfer',
-    //     payload:
-    //     {
-    //       transactionId:
-    //         'ce05d4536ee324f4aa8f76c28a1e1ed9de3055519fe2eef542963830d4b26f9b',
-    //       actionIndex: 0,
-    //       account: 'eosio.token',
-    //       name: 'transfer',
-    //       authorization: [{ actor: 'sunshine1212', permission: 'owner' }],
-    //       data:
-    //       {
-    //         from: 'sunshine1212',
-    //         to: 'eosmaxiodice',
-    //         quantity: '0.1000 EOS',
-    //         memo: '1-50-cpuemergency-'
-    //       }
-    //     }
-    //   }*/
-    //   ],
-    //   blockInfo: {}
-    // }
-
-    return {} as Block
+    if (queuedBlockNumber > blockNumber) {
+      console.log(`Returning default block for num ${blockNumber}`)
+      return defaultBlock
+    } else if (queuedBlockNumber === blockNumber) {
+      this.blockQueue.shift()
+      return queuedBlock
+    } else {
+      throw new Error(
+        `Could not find block number ${blockNumber} in queue containing blocks ${this.blockQueue.map(
+          (x) => x.blockInfo.blockNumber
+        )}`
+      )
+    }
   }
 
   public async getHeadBlockNumber(): Promise<number> {
-    await waitUntil(() => this.headBlockNumber > 0)
-
+    await waitUntil(() => this.headBlockNumber !== 0)
     return this.headBlockNumber
   }
 
   public async getLastIrreversibleBlockNumber(): Promise<number> {
-    await waitUntil(() => this.headBlockNumber > 0)
-
+    await waitUntil(() => this.lastIrreversibleBlockNumber !== 0)
     return this.lastIrreversibleBlockNumber
   }
+
+  // With dfuse, you don't need to resolve forks
+  protected async resolveFork() {}
 }
 
 async function waitUntil(condition: () => boolean, timeout: number = 125) {
